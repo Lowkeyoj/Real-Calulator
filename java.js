b16f51ed168ad95e8e586cd592da262f1464da14
Lowkeyoj <rockyojaji@gmail.com>
2022-11-07 20:36:54 -0800

    Skeleton

---
+++ java.js
@@ -0,0 +1,19 @@
+let add = function(num, num2){
+   return  num + num2;
+};
+
+let divide = function(num, num2){
+   return x / y; 
+};
+
+let multiply = function(num, num2){
+    return m * n;
+}
+
+let power = function(num, num2){
+    return Math.pow(num, num2);
+}
+
+let subtract = function(num, num2){
+    return num - num2
+}
