fd97b02d59f8da092d6944005de50b4dd82d5a0e
Lowkeyoj <rockyojaji@gmail.com>
2022-11-12 17:07:40 -0800

    only works with one operator at a time

--- java.js
+++ java.js
@@ -2,11 +2,15 @@
 const input = document.getElementById('input-text');
 
 
-
+//let num = 0;
+//let testcase = input.value;
+//testcase = testcase.replace('+', ' ');
+//testcase = testcase.split(' ');
+//testcase = testcase.map(Number)
 
 
 let add = function(total, num){
-  return  total + num;
+ return total + num;
 };
 
 let divide = function(total, num){
@@ -40,5 +44,40 @@ let dl = () =>{
 }
 
 let results = () =>{
-  input.value = eval(input.value);
+  if(input.value.includes('+')){
+    let plus = input.value;
+    plus = plus.replace('+', ' ');
+    plus = plus.split(' ');
+    plus = plus.map(Number);
+    plus = plus.reduce(add);
+    input.value = plus;
+  }else if(input.value.includes('-')){
+    let sub = input.value;
+    sub = sub.replace('-', ' ');
+    sub = sub.split(' ');
+    sub = sub.map(Number);
+    sub = sub.reduce(subtract);
+    input.value = sub;
+  }else if(input.value.includes('*')){
+    let multi = input.value;
+    multi = multi.replace('*', ' ');
+    multi = multi.split(' ');
+    multi = multi.map(Number);
+    multi = multi.reduce(multiply);
+    input.value = multi;
+  }else if(input.value.includes('/')){
+    let division = input.value;
+    division = division.replace('/', ' ');
+    division = division.split(' ');
+    division = division.map(Number);
+    division = division.reduce(divide);
+    input.value = division;
+  }else if(input.value.includes('^')){
+    let pow = input.value;
+    pow = pow.replace('^', ' ');
+    pow = pow.split(' ');
+    pow = pow.map(Number);
+    pow = power(pow[0],pow[1]);
+    input.value = pow;
+  }
 }
